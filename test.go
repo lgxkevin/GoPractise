6ade8ea101dcae789921bcf42821558d06d621ad
lgxkevin <lgx9587@gmail.com>
2020-03-29 18:12:12 +1300

    Practise variables

---
+++ test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var k int = 99
+
+//var(
+//	firstname string = "Kevin"
+//	lastname string = "Kevin"
+//	age int = 3
+//)
+func main() {
+	//fmt.Println("Message: ", testMessage.TextMessage)
+
+	//var i int = 42
+	var h int = 30
+	var w string
+	w = strconv.Itoa(h)
+	fmt.Printf("%v, %T\n", w, w)
+	fmt.Printf("%v, %T\n", k, k)
+}
